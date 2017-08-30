bdf0af75b95e24d44125eb1e58769e54bf142749
cahlering <cahlering@gmail.com>
2017-08-29 22:59:13 -0700

    Configure worker enablement.

--- cloud/ReminderConfig.js
+++ cloud/ReminderConfig.js
@@ -6,14 +6,16 @@ let ReminderConfigObject = Parse.Object.extend(className);
 let existingCheck = new Parse.Query(ReminderConfigObject);
 existingCheck.count().then(function(reminderCt) {
     if (reminderCt === 0) {
-        new ReminderConfigObject({"requestedTime":moment().subtract(12, 'hours')}).save();
+        return new ReminderConfigObject({"requestedTime":moment().subtract(12, 'hours')}).save();
     }
+}).then(function(reminderConfig) {
+    console.log("Seeded reminder: " + reminderConfig.id);
 });
 
 exports.getLastReminderTime = function() {
     let reminderConfigQuery = new Parse.Query(ReminderConfigObject);
     return reminderConfigQuery.descending("requestedTime").first().then(function (result) {
-        return result.get("requestedTime");
+        return result.get("requestedTime").getTime();
     });
 };
 
