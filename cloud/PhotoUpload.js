559be36b279fff31d0f4f745f4e63af6c8afdc71
cahlering <cahlering@gmail.com>
2017-08-29 21:58:56 -0700

    Configure worker enablement.

--- cloud/PhotoUpload.js
+++ cloud/PhotoUpload.js
@@ -429,14 +429,14 @@ Parse.Cloud.define("remind", function(request, response) {
 exports.checkReminder = function() {
     var reminderDateStart = moment().startOf("day").toDate();
     var reminderDateEnd = moment().endOf("day").toDate();
-    console.log("Remind " + deviceId + " from " + reminderDateStart + " to " + reminderDateEnd);
+    console.log("Reminders from " + reminderDateStart + " to " + reminderDateEnd);
     return new Parse.Query(PhotoUploadObject).greaterThan(REMIND_DATE_FIELD, reminderDateStart).lessThan(REMIND_DATE_FIELD, reminderDateEnd);
 }
 
 exports.checkReminderByDevice = function(deviceId) {
     var reminderDateStart = moment().startOf("day").toDate();
     var reminderDateEnd = moment().endOf("day").toDate();
-    console.log("Remind " + deviceId + " from " + reminderDateStart + " to " + reminderDateEnd);
+    console.log("Reminders for " + deviceId + " from " + reminderDateStart + " to " + reminderDateEnd);
     return getPhotoUploadQueryForDevice(deviceId).greaterThan(REMIND_DATE_FIELD, reminderDateStart).lessThan(REMIND_DATE_FIELD, reminderDateEnd);
 }
 
