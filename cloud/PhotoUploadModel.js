df4d164e1091185f199382b0dda8e79f8d4c2792
cahlering <cahlering@gmail.com>
2018-06-05 23:16:26 -0700

    Updated logging for flash-backed images.

--- cloud/PhotoUploadModel.js
+++ cloud/PhotoUploadModel.js
@@ -221,7 +221,7 @@ exports.clusterUnclusteredForDevice = clusterUnclusteredForDevice;
 exports.checkFlashback = function(deviceId, lookbackNum, lookbackPeriod) {
   var lookbackStart = moment().subtract(lookbackNum, lookbackPeriod).startOf("day").valueOf();
   var lookbackEnd = moment().subtract(lookbackNum, lookbackPeriod).endOf("day").valueOf();
-  console.log("Flashback from " + lookbackStart + " to " + lookbackEnd);
+  console.log("Flashback for " + deviceId + " from " + lookbackStart + " to " + lookbackEnd);
   return getPhotoUploadQueryForDevice(deviceId).greaterThan(TAKEN_TIMESTAMP_FIELD, lookbackStart).lessThan(TAKEN_TIMESTAMP_FIELD, lookbackEnd);
 };
 
